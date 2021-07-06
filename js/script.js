99d5ccaa247cc42b0f69971a47a70755f894ba23
Jacob Molsby <jacobmolsby@hotmail.se>
2021-07-06 20:34:11 +0000

    innitial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    // |.click function| works the same as |.on('click', function())|
+    $("#stream1_btn").click(function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+}); 
